2daa9965f631fa81ecfd59032b8428703abe1525
alisavu <ahvu@ucsd.edu>
2022-07-12 08:16:23 -0700

    markdown-parse commit

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,7 +11,7 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
+        while((currentIndex < markdown.length())) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
@@ -19,6 +19,8 @@ public class MarkdownParse {
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
+        
+
 
         return toReturn;
     }
@@ -29,5 +31,6 @@ public class MarkdownParse {
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
+
     }
 }
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,4 +1,11 @@
 import static org.junit.Assert.*;
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 import org.junit.*;
 public class MarkdownParseTest {
 
@@ -6,5 +13,14 @@ public class MarkdownParseTest {
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+
+    @Test
+    public void getLinksFile1() throws IOException {
+        List<String> stringList = List.of("https://something.com", "some-thing.html");
+        String fileName = Files.readString(Path.of("/Users/alisavu/Documents/GitHub/markdown-parser-1/test-file.md"));
+        assertEquals(MarkdownParse.getLinks(fileName), stringList);
+
+    }
+    
 
 }
